274d9f04b912c6a047cf8c5bbf1880d5a0204888
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2024-09-20 02:56:00 +0300

    file manager

--- apps/fileManager/main.lua
+++ apps/fileManager/main.lua
@@ -65,31 +65,63 @@ local function openFolder(dir)
   -- local w, h = term.getSize()
   local FileList = fs.list(dir)
   for i, file in ipairs(FileList) do
-    local typeStr = ""
+    local typeIndex = 0
     if (fs.isDir(dir .. file)) then
-      typeStr = "dir "
+      typeIndex = 0
     else
-      typeStr = "file"
+      typeIndex = 1
     end
       table.insert(filesTable,
         fileListFrame:addFrame()
-          :setSize("parent.w", 2)
+          :setSize("parent.w", 3)
           :setBackground(colors.white)
-          :setPosition(1, (i - 1) * 2 + 1)
+          :setPosition(1, (i - 1) * 3 + 1)
           :onClick(
             function()
-              if (typeStr == "dir ") then
+              if (typeIndex == 0) then
                 openFolder(path .. file .. "/")
               end
             end)
       )
 
       filesTable[i]:addLabel()
-        :setPosition(4, 1)
+        :setPosition(5, 1)
         :setText(file)
         :setFontSize(1)
         :setBackground(colors.white)
         :setForeground(colors.black)
+
+      filesTable[i]:addLabel()
+        :setPosition(5, 2)
+        :setText(file)
+        :setFontSize(1)
+        :setBackground(colors.white)
+        :setForeground(colors.gray)
+
+      filesTable[i]:addPane()
+        :setSize("parent.w", 1)
+        :setPosition(1, 3)
+        :setBackground(colors.white, "-", colors.lightGray)
+
+      if (typeIndex == 0) then
+        filesTable[i]:addLabel()
+          :setPosition(2, 1)
+          :setSize(2, 2)
+          -- :setText("\136\142\n\130\131")
+          :setText(" \144\n\143\133")
+          -- :setText("\141\142\n\143\143")
+          :setForeground(colors.yellow)
+      else
+        filesTable[i]:addLabel()
+          :setPosition(2, 1)
+          :setSize(2, 2)
+          -- :setText("\136\142\n\130\131")
+          :setText(" \144\n\143\133")
+          :setForeground(colors.blue)
+      end
+
+
+
 
     -- fileListFrame:addLabel()
     --   :setPosition(1, backButtonFlag + i)
